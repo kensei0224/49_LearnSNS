0e3e708fcdb4272c3be2906db2972f15250ce8c4
田村謙征 <>
2019-01-30 10:16:14 +0800

    削除処理実装

--- delete.php
+++ delete.php
@@ -1,4 +1,16 @@
 <?php
 
+require('dbconnect.php');
+
+//消去する投稿IDを取得
+$feed_id = $_GET['feed_id'];
+
+//削除
+//DELETE FROM テーブル WHERE
+$sql = 'DELETE FROM `feeds`WHERE`id` = ?';
+$data = [$feed_id];
+$stmt = $dbh->prepare($sql);
+$stmt->execute($data);
+
 header("Location: timeline.php");
 exit();
